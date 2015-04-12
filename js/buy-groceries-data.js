3b93c9facb6287d815d988f0cba6444b1dc0be02
James Ginns <starvagrant@yahoo.com>
2015-04-11 20:04:49 -0500

    Form able to add an element (debug needed)
    I'm satsified with the logic and methods of
    the jQuery program, which is supposed to make
    a form that interactively expands / contracts
    to enter data in a MySQL database. Trying to add
    form fields on the fly.
    
    I'm happy with the logic and methods of the program
    But currently it's completely broken and needs
    debugging. I'm not sure how to debug jQuery right
    now

--- js/buy-groceries-data.js
+++ js/buy-groceries-data.js
@@ -1,10 +1,50 @@
+// to do: there should be a button that adds input fields #adder
+	// would like said input button to be hidden
+	// said button should add a list item to the grocery list.
+	// this should have the properties of other li input items: 
+	// readonly, and be removed by a jQuery function.  
+// below needs more comments 
 $(':header').addClass('headline');
+var addInput = $('#adder');
+addInput.hide();
+$('#add-button').on('click', function() {
+	addButton.show();	
+}
 
-$('li#submit-button').hide().fadeIn(1500);
+addInput.on('blur', function() {
+	// addInput already a jquery object
+	// create a new Jquery list item
+	var initValue = this.attr('value'); 		// get the value in the form
+	var $listItem = $('<li><input type="text" value="' + initValue + '" /></li>');
+	var lastField = $('li').not('#submit-button').last();
+	lastField.after($listItem);
+//	this.parent();
+/*	
+	var listNode = document.createElement('li');
+	var inputNode = document.createElement('input');
+	inputNode.
+	ListItem.
+*/
+}
+
+// $('li input').hide().fadeIn(700);
 /* $('li:not("#submit-button")').hide().fadeIn(1500); 
 $('li').not('#submit-button').hide().fadeIn(1500);
 */
+/*$('li').not('button').on('click', function() {
+	$(this).remove();
+}
+$('#add-button').on('click' , function() {
+	$('#adder').show();
+}
+$('#adder').on('blur', function () {
+	var inputField = $(this);
+	var listItem = $(this).parent();
 
-$('li').not('#submit-button').on('click', function() {
-  $(this).remove();
+	var inputText =	'<li id="four"><input type="text" /></li>'
+}
+$function
+$('#add-button').on('click', function() {
+	$(this).parent().append
 });
+*/
