f75d78a8b08b6742e52f2b52fea572dd008bab56
James Ginns <starvagrant@yahoo.com>
2015-06-02 23:22:55 -0500

    Initial code for Habit form ajax
    This commit is one where I'm stuck, not finished. I wanted to turn
    my habit program into one incorporating ajax. I tried to do this
    in jquery, based on a previous program's success. I can't debug
    it. Perhaps I should try doing it in plain javascript instead.

---
+++ habit/habit-complete.php
@@ -0,0 +1,41 @@
+<?php
+if(isset($_POST)) {
+	ob_start();
+		var_dump($_POST);
+		$a = ob_get_contents();
+		error_log($a);
+	ob_end_clean();
+} else {
+	error_log('post_empty');
+}
+if(isset($_GET)) {
+	ob_start();
+
+	var_dump($_GET);
+	$a = ob_get_contents();
+	error_log($a);
+
+	ob_end_clean();
+}
+/*
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+	<meta charset="utf-8">
+<title></title>
+<style>
+// {	text-indent: 4em;	} 
+</style>
+</head>
+
+<body>
+ <pre>
+	<?php var_dump($a); ?>
+	yoyoyo
+ </pre>
+
+</body>
+</html>
+*/
+?>
