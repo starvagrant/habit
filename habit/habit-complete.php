1384417fe6fda3c28944049989da06718b56b44a
James Ginns <starvagrant@yahoo.com>
2015-06-05 00:17:08 -0500

    Found Bug - script does not work on dynamic page
    Based on my testing, a jquery/ajax script that I've been working
    on stops working after it's been made dynamically. The page successfully
    generates dynamically. The scripting works fine accept the ajax
    requests. Ajax is failing and I don't know how to test for errors.

--- habit/habit-complete.php
+++ habit/habit-complete.php
@@ -8,35 +8,6 @@ if(isset($_POST)) {
 } else {
 	error_log('post_empty');
 }
-if(isset($_GET)) {
-	ob_start();
 
-	var_dump($_GET);
-	$a = ob_get_contents();
-	error_log($a);
-
-	ob_end_clean();
-}
 print("this is a response");
-/*
-<!DOCTYPE html>
-<html lang="en-us">
-
-<head>
-	<meta charset="utf-8">
-<title></title>
-<style>
-// {	text-indent: 4em;	} 
-</style>
-</head>
-
-<body>
- <pre>
-	<?php var_dump($a); ?>
-	yoyoyo
- </pre>
-
-</body>
-</html>
-*/
 ?>
