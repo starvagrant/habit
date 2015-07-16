75040fa92d7d8bc08f064468bf50df9985941a60
James Ginns <starvagrant@yahoo.com>
2015-07-15 23:49:25 -0500

    Habit complete can process multiple items
    My habit-complete script can now take clusters of three form items
    (the three inputs a habit will submit from the form) and input them
    all into a database.

--- habit/habit-complete.php
+++ habit/habit-complete.php
@@ -1,34 +1,57 @@
 <?php
 if(isset($_POST)) {
 require 'login.php';
-print("ajax result is: "); 					// response sent upon ajax request
+$ajax = "ajax result is:\n"; 					// response sent upon ajax request
 
 $dsn = 'mysql:host=localhost;dbname=habit';
-try { $pdo = new PDO($dsn, $db_username, $db_password); } 
+try { $pdo = new PDO($dsn, $db_username, $db_password); }
 catch (PDOException $e) { $error = $e->getMessage(); die("$error"); }
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$ajax .= "database connected\n";
 
-$keys_from_post = array_keys($_POST); 				// get the number of form names by array keys 
-foreach($keys_from_post as $key => $value) { error_log($value); }
-preg_match('/\d+/', $keys_from_post[0], $matches);
-$number = $matches[0];
+$keys_from_post = array_keys($_POST); 				// get the number of form names by array keys
+													// note, foreach goes through post by numeric keys
+													// so every third key will be what we're looking for
+foreach($keys_from_post as $key => $value) {
 
-$habit = $_POST["habit$number"];
-if (isset($_POST["complete$number"])){
-	$complete = 1;			 			// by convention: 1=true, 0=false	
-} else {
-	$complete = 0;
-}
-if (isset($_POST["priority$number"])){
-	$priority = 1;			 			
-} else {
-	$priority = 0;
-}
-// habit, complete, priority assigned
+	// match the digits in the keys
+	if ($key % 3 === 0){
+		preg_match('/\d+/', $value, $matches);
+		$number = $matches[0];
+	$habit = $_POST["habit$number"];
+	if (isset($_POST["complete$number"])){
+		$complete = 1;			 			// by convention: 1=true, 0=false
+	} else {
+		$complete = 0;
+	}
+	if (isset($_POST["priority$number"])){
+		$priority = 1;
+	} else {
+		$priority = 0;
+	}
+	// habit, complete, priority assigned
+	$sql_array[] = array($complete, $priority, $habit);
+
+	} // end if
+
+} // end foreach (sql_array populated)
 
 $sql_update = "UPDATE habit_tracker SET completion = completion + ? , priority = priority + ? WHERE habit_name=?; ";
-if( !$statement = $pdo->prepare($sql_update) ) print ("statement not prepared");
-if( !$statement->execute(array($priority, $complete, $habit)) ) print(" statement not executed ");
+if( !$statement = $pdo->prepare($sql_update) ) print ($ajax . " statement not prepared");
+
+// $ajax = $statement->queryString;
+
+foreach ($sql_array as $key => $array){
+		$ajax .= "\n" . $key . " is the outer key";
+	foreach($array as $check_key => $checks){
+		$ajax .= "\n" . $check_key . " is the inner key";
+		$ajax .= "\n" . $checks . " is the value";
+	}
+	if( !$statement->execute($array) )  $ajax .= "\n" . $statement->errorCode() . " execute problem" ;
+}
+
+if ($pdo->errorCode !== null) $ajax .= "\n" . $pdo->errorCode . " db handle problem";
+echo $ajax;
 
 } // end POST processing
 ?>
